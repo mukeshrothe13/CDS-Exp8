b929f225affa9fa4e2e37ad490e78dbad49e2bb3
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-23 09:54:59 +0530

    8A_matrix.cpp

---
+++ 8A_matrix.cpp
@@ -0,0 +1,34 @@
+//Mukesh Rothe  //23070123089  //EXP8
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols, i, j;
+
+    // Take input for number of rows and columns
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int matrix[rows][cols];
+
+    cout << "Enter elements of matrix:" << endl;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            cout << "Enter element [" << i + 1 << "," << j + 1 << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Entered Matrix:" << endl;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+            if (j == cols - 1)
+                cout << endl;
+        }
+    }
+
+    return 0;
+}
